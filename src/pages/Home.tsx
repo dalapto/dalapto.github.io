e94289a532746c632a2cbf36fddeec7bbb42ea46
dalapto <dmcalister11@outlook.com>
2023-09-14 14:58:16 +0100

    icons and links for packages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Grid, Tooltip, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { Container } from '@mui/system';
 import React from 'react';
@@ -9,6 +9,12 @@ function Home() {
 		{ name: 'About Me', route: 'about', img: '../src/img/tile-aboutmehome.png' },
 		{ name: 'Projects', route: 'projects', img: '../src/img/tile-m2home.png' },
 		{ name: 'Blog', route: 'blog', img: '../src/img/tile-bloghome.png' },
+	];
+	const tooltips = [
+		{ text: 'Vite', link: 'https://vitejs.dev/', img: '../src/img/logo-vite.png' },
+		{ text: 'React', link: 'https://react.dev/', img: '../src/img/logo-react.webp' },
+		{ text: 'TypeScript', link: 'https://www.typescriptlang.org/', img: '../src/img/logo-ts.png' },
+		{ text: 'GitHub Pages', link: 'https://pages.github.com/', img: '../src/img/logo-github.png' },
 	];
 	return (
 		<div className="App">
@@ -23,7 +29,21 @@ function Home() {
 					<Typography variant="h4">
 						{/* icons & links for each package */}
 						<p>
-							Making websites is fun.<br></br>So I made this one.<br></br>It uses <span>vite</span>, <span>react-ts</span>, and <span>gh-pages</span>.
+							Making websites is fun.<br></br>So I made this one. <br></br>
+							<Typography fontSize={'1.6rem'} fontFamily={'monospace'}>
+								<span>{'It uses'}</span>
+								{tooltips.map((tooltip) => (
+									<span key={tooltip.text}>
+										{tooltip.text != tooltips[tooltips.length - 1].text ? ' ' : ' and '}
+										<Tooltip followCursor={true} placement="top" title={<img width={25} height={25} src={tooltip.img}></img>}>
+											<a href={tooltip.link} target="_blank" rel="noopener noreferrer">
+												<span>{tooltip.text}</span>
+											</a>
+										</Tooltip>
+										{tooltip.text != tooltips[tooltips.length - 1].text ? ', ' : '.'}
+									</span>
+								))}
+							</Typography>
 						</p>
 					</Typography>
 					<span>Feel free to look around.</span>
