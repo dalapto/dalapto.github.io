bb8383a0269c17dec9bb904c9e86a206fa689817
David McAlister <your-personal-email@example.com>
2026-04-30 12:15:27 +0100

    fade for page changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { animated, useTransition } from '@react-spring/web';
 import React, { useEffect } from 'react';
 import { Route, Routes, useLocation } from 'react-router';
 import './App.css';
@@ -7,7 +8,8 @@ import { NavBar } from './components/layout/NavBar/NavBar';
 import { navRoutes } from './constants/routes';
 
 function App() {
-	const currentPage = useLocation().pathname;
+	const location = useLocation();
+	const currentPage = location.pathname;
 	const currentPageSlice = currentPage.slice(1);
 
 	useEffect(() => {
@@ -18,19 +20,37 @@ function App() {
 		}`;
 	}, [currentPageSlice]);
 
+	const transitions = useTransition(location, {
+		keys: (loc) => loc.pathname,
+		from: { opacity: 0, transform: 'translateX(50px)' },
+		enter: { opacity: 1, transform: 'translateX(0px)' },
+		leave: { opacity: 0, transform: 'translateX(-20px)' },
+		config: (_item, _index, phase) => {
+			if (phase === 'enter') return { duration: 900 };
+			if (phase === 'leave') return { duration: 200 };
+			return { duration: 0 };
+		},
+		exitBeforeEnter: true,
+		delay: 500,
+	});
+
 	return (
 		<div className='App'>
 			<NavBar currentPage={currentPage} navRoutes={navRoutes} />
 			<div className='routes-container'>
-				<Routes>
-					{navRoutes.map((r) => (
-						<Route
-							key={r.route}
-							path={r.route}
-							element={r.component ? r.component() : <PageInConstruction />}
-						/>
-					))}
-				</Routes>
+				{transitions((style, loc) => (
+					<animated.div style={style} className='page-transition-wrapper'>
+						<Routes location={loc}>
+							{navRoutes.map((r) => (
+								<Route
+									key={r.route}
+									path={r.route}
+									element={r.component ? r.component() : <PageInConstruction />}
+								/>
+							))}
+						</Routes>
+					</animated.div>
+				))}
 			</div>
 			<FooterBar />
 		</div>
