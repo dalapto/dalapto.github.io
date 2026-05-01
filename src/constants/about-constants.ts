8be29cf71f75944f0bcdd987fb63f3194d6902f4
David McAlister <your-personal-email@example.com>
2026-05-01 11:20:58 +0100

    adding content to about page

--- src/constants/about-constants.ts
+++ src/constants/about-constants.ts
@@ -16,4 +16,44 @@ const podcasts = [
 	},
 ];
 
-export { podcasts };
+const blurb = '(how I became a Software Engineer)';
+
+const uni_translations = {
+	uni: 'University of Edinburgh',
+	subject: 'BSc Computer Science',
+	grade: '1st',
+	before_uni: [
+		'My first real taste of the SDLC was in school.',
+		'Using VB6, I built a bookkeeping app for a local book shop, documenting the entire process.',
+		'I spent a couple days writing out two dozen if-statements, just to switch between currencies.',
+		'',
+		'Thankfully, that code has been lost to time.',
+		'',
+		'After the fun of building my own app, I switched my university application from Chemistry to Computer Science.',
+		'This meant next year was getting my Maths A-level (which I had skipped) to meet requirements...',
+		'',
+		'...so my gap year was spent between a call centre job and trigonometry! 📐',
+	],
+	at_uni: [
+		'I fumbled first and second year of uni, having to resit multiple exams. Every course was boring and hard. I often fell asleep in morning lectures. But those two years taught me the life-skills I needed to study well.',
+		'So, come year 3, I knew how to knuckle down. It helped that now, not only did the courses go towards my final grade, but I was also able to choose areas I was actually interested in. It mattered now.',
+		"Then, come March 2020 - everything stopped. I flew back home, unsure what this coronavirus pandemic meant. When it became obvious this was no Spring thing, I cancelled the flat lease, unsure when I would return. My brother came to sort through all my things I had left behind. I wouldn't see them for another 2 years.",
+		'It was a good time to finish my degree. I had swapped socialising for studying and besides, the Informatics department was well equipped to deliver and assess us virtually. Even if lots of lecturers recycled their old lecture recordings...',
+	],
+	recyclotron: {
+		title: 'Recyclotron',
+		content:
+			"There was also a 'build-us-a-robot' course. Our group decided to make an auto-sorting bin. We trained our NN AI on images of rubbish. As I had 4 other courses on-the-go, I decided to manage the team of ten and writeup reports. We had some smart cookies.",
+	},
+	litter: {
+		title: 'Litter',
+		subtitle: 'Location-based Social Media App',
+		content1:
+			'One course in third year was creating a rules-based pathfinding AI in Java. This was a hoot and I wondered if I could expand on this for my final year project...',
+		content2: 'Luckily for me the course lecturer had such a project!',
+		content3:
+			'I was to create a location-based social media app, using Kotlin. I go into more detail here. It was a lot of fun.',
+	},
+};
+
+export { blurb, podcasts, uni_translations };
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { ImageTextLayout } from '../../components/display/ImageTextLayout/ImageTextLayout';
 import { ParallaxCanvas } from '../../components/display/ParallaxCanvas/ParallaxCanvas';
+import { TextList } from '../../components/display/TextList/TextList';
 import { TileList } from '../../components/display/TileList/TileList';
-import { podcasts } from '../../constants/about-constants';
+import {
+	blurb,
+	podcasts,
+	uni_translations,
+} from '../../constants/about-constants';
 import './About.css';
 
 function About() {
@@ -19,49 +24,44 @@ function About() {
 				<div className='about-page'>
 					<div className='about-header'>
 						<h1 className='about-title'>about</h1>
-						<p className='about-subtitle'>(me)</p>
+						<p className='about-subtitle'>{blurb}</p>
 					</div>
-					<TileList
-						items={podcasts}
-						imgSize='20vmin'
-						direction={'row'}
-						imgPathPrefix='/img/podcasts/'
-						title='Favourite Podcasts:'
-						className='podcast-tiles'
-					/>
-					<TileList
-						items={podcasts}
-						imgSize='20vmin'
-						direction={'row'}
-						imgPathPrefix='/img/podcasts/'
-						title='Favourite Podcasts:'
-						className='podcast-tiles'
-					/>
+					<span
+						style={{
+							maxWidth: '70%',
+							alignSelf: 'center',
+							backgroundColor: 'rgba(130, 60, 55, 0.95)',
+							padding: '1rem',
+							borderRadius: '0.5rem',
+						}}
+					>
+						<TextList strings={uni_translations.before_uni} />
+					</span>
 				</div>
 			</ParallaxCanvas>
-		<div className='about-image-text-section'>
-			<ImageTextLayout
-				image={{
-					src: '/img/about/arch-move.png',
-					alt: 'Me standing in an archway looking into sunset in lake district.',
-				}}
-				imageColumnWidth='45%'
-				additionalContent={
-					<TileList
-						items={podcasts}
-						imgSize='20vmin'
-						direction={'row'}
-						imgPathPrefix='/img/podcasts/'
-						title='Favourite Podcasts:'
-						className='podcast-tiles'
-					/>
-				}
-			>
-				<p>{'hey'}</p>
-				<p>{'I make things, like this website'}</p>
-				<p>{'ye'}</p>
-			</ImageTextLayout>
-		</div>
+			<div className='about-image-text-section'>
+				<ImageTextLayout
+					image={{
+						src: '/img/about/arch-move.png',
+						alt: 'Me standing in an archway looking into sunset in lake district.',
+					}}
+					imageColumnWidth='45%'
+					additionalContent={
+						<TileList
+							items={podcasts}
+							imgSize='20vmin'
+							direction={'row'}
+							imgPathPrefix='/img/podcasts/'
+							title='Favourite Podcasts:'
+							className='podcast-tiles'
+						/>
+					}
+				>
+					<p>{'hey'}</p>
+					<p>{'I make things, like this website'}</p>
+					<p>{'ye'}</p>
+				</ImageTextLayout>
+			</div>
 		</>
 	);
 }
